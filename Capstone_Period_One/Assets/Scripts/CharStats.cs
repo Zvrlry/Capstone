2e65da77780f44e365b66534df8a07a348ec627c
Devon Dunham <itisdevondunham@gmail.com>
2021-11-08 12:12:59 -0500

    Leveling Stats: In Progress

---
+++ Capstone_Period_One/Assets/Scripts/CharStats.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+
+    [Header("Name and Experience")]
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int baseEXP = 1000;
+
+    [Header("HP and MP")]
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP;
+
+    [Header("Strength, Defence, Weapon, and Armor")]
+    public int strength;
+    public int defence;
+    public int weaponPower;
+    public int armor;
+    public string equippedWeapon;
+    public string equippedArmor;
+    public Sprite charImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        expToNextLevel = new int[maxLevel]; // setting the index of exp to next level to our max level
+        expToNextLevel[1] = baseEXP;
+
+        for (int i = 2; i < expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i - 1] * 1.05f); // multiplying each level by 1.05
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K))
+        {
+            AddExp(500);
+        }
+    }
+
+    public void AddExp(int expToAdd)
+    {
+        currentEXP += expToAdd;
+
+        if (currentEXP > expToNextLevel[playerLevel])
+        {
+            currentEXP -= expToNextLevel[playerLevel]; // subtracting current exp
+
+            playerLevel++; // adding one more player level
+        }
+    }
+}
