538ecbe655214baac39baad4a2846dd62a5db7f4
Cris3061 <91195919+Cris3061@users.noreply.github.com>
2021-11-13 15:17:44 -0500

    In game Mneu: In progress
    Currently stuck on a bug near the end that i've been trying to fix for awhile

--- Capstone_Period_One/Assets/Scripts/GameMenu.cs
+++ Capstone_Period_One/Assets/Scripts/GameMenu.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameMenu : MonoBehaviour
 {
     public GameObject theMenu;
+    public GameObject[] windows;
+
+    private CharStats[] playerStats;
+
+    public Text[] nameTest, hptext, mpText, lvltext, expText;
+    public Slider[] expSlider;
+    public Image[] charImage;
+    public GameObject[] charStatHolder;
+    public GameObject[] statusButton;
     
     // Start is called before the first frame update
     void Start()
@@ -19,13 +29,78 @@ public class GameMenu : MonoBehaviour
         {
             if(theMenu.activeInHierarchy)
             {
-                theMenu.SetActive(false);
-                GameManager.instance.gameMenuOpen = false;
+                // theMenu.SetActive(false);
+                //GameManager.instance.gameMenuOpen = false;
+
+                CloseMenu();
             } else
             {
                 theMenu.SetActive(true);
+                UpdateMainStats();
                 GameManager.instance.gameMenuOpen = true;
             }
         }
+    }
+
+    public void UpdateMainStats()
+    {
+        playerStats = GameManager.instance.playerStats;
+
+        for(int i = 0; 1 < playerStats.Length; i++)
+        {
+            if(playerStats[i].gameObject.activeInHierarchy)
+            {
+                charStatHolder[i].SetActive(true);
+
+                nameTest[i].text = playerStats[i].charName;
+                hptext[i].text = "HP: " + playerStats[i].currentHP + "/" + playerStats[i].maxHP;
+                mpText[i].text = "MP: " + playerStats[i].currentMP + "/" + playerStats[i].maxMP;
+                lvltext[i].text = "Lvl: " + playerStats[i].playerLevel;
+                expText[i].text = "" + playerStats[i].currentEXP + "/" + playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                expSlider[i].maxValue = playerStats[i].expToNextLevel[playerStats[i].playerLevel];
+                expSlider[i].value = playerStats[i].currentEXP;
+                charImage[i].sprite = playerStats[i].charImage;
+            } else{
+                charStatHolder[i].SetActive(false);
+            }
+        }
+    }
+    public void ToggleWindow(int windowNumber)
+    {
+        UpdateMainStats();
+
+        for(int i = 0; i < windows.Length; i++)
+        {
+            if(i == windowNumber)
+            {
+                windows[i].SetActive(!windows[i].activeInHierarchy);
+            } else
+            {
+                windows[i].SetActive(false);
+            }
+        }
+    }
+
+    public void CloseMenu()
+    {
+        for(int i = 0; i < windows.Length; i++)
+        {
+            windows[i].SetActive(false);
+        }
+
+        theMenu.SetActive(false);
+
+        GameManager.instance.gameMenuOpen = false;
+    }
+
+    public void OpenStatus()
+    {
+        UpdateMainStats();
+
+        for(int i = 0; i < statusButton.Length; i++)
+        {
+            statusButton[i].SetActive(playerStats[i].gameObject.activeInHierarchy);
+            statusButton[i].GetComponentInChildren<Text>().text = playerStats[i].charName;
+        } 
     }
 }
