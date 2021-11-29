91e8100d48581a3cff8ec320d7d1263102e15c89
Devon Dunham <itisdevondunham@gmail.com>
2021-11-29 12:20:37 -0500

    Inventory: In Progress

--- Capstone_Period_One/Assets/Scripts/Item.cs
+++ Capstone_Period_One/Assets/Scripts/Item.cs
@@ -2,7 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item : MonoBehaviour { 
+public class Item : MonoBehaviour
+{
+
     [Header("Item Type")]
     public bool isItem;
     public bool isWeapon;
@@ -14,25 +16,24 @@ public class Item : MonoBehaviour {
     public int value;
     public Sprite itemSprite;
 
-    [Header("Item Details")]
+    [Header("Item Impact")]
     public int amountToChange;
-    public bool affectHP, affectMP, affectStr;
+    public bool affectHP, affectMP, affectStrength;
 
     [Header("Weapon/Armor Details")]
     public int weaponStrength;
-
     public int armorStrength;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
