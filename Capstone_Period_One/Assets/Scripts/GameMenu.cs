9139c1e1758071afb556785727ab9db1d64706c0
Yiannis04 <Yiannis1.ft@gmail.com>
2021-12-04 18:13:54 -0500

    Money and Shops: In Progress

--- Capstone_Period_One/Assets/Scripts/GameMenu.cs
+++ Capstone_Period_One/Assets/Scripts/GameMenu.cs
@@ -29,6 +29,8 @@ public class GameMenu : MonoBehaviour
     public GameObject itemCharChoiceMenu;
     public Text[] itemCharChoiceNames;
 
+    public Text goldText;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -86,6 +88,8 @@ public class GameMenu : MonoBehaviour
                 charStatHolder[i].SetActive(false);
             }
         }
+
+        goldText.text = GameManager.instance.currentGold.ToString() + "g";
     }
     public void ToggleWindow(int windowNumber)
     {
